f9cb1afd3a6f9e05e7948865b8c72e85966715c0
rajgohil07 <raj.famous007@gmail.com>
2022-04-27 13:49:22 +0530

    refactor the code

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,21 +1,10 @@
-import { Context, Query, Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { CheckConnectionDTO } from './dto/checkConnectionDTO';
 
 @Resolver()
 export class UserResolver {
   @Query(() => CheckConnectionDTO)
-  checkServer(@Context() ctx: any): CheckConnectionDTO {
-    ctx.req.session.UserID = 'raj';
-    return { connectionStatus: 'connected with graphql' };
-  }
-
-  @Query(() => CheckConnectionDTO)
-  checkSession(
-    @Context('session') session: any,
-    @Context('req') req: any,
-  ): CheckConnectionDTO {
-    console.log(req, '\n\n\n ctx is here');
-    console.log(session, '\n\n\n ctx is here');
+  checkServer(): CheckConnectionDTO {
     return { connectionStatus: 'connected with graphql' };
   }
 }
