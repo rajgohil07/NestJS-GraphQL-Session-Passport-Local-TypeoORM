e8a0b45e1b3d964d52cffaae59682d594a4362ad
rajgohil07 <raj.famous007@gmail.com>
2022-04-27 13:54:13 +0530

    removed express-session

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
 import dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import session from 'express-session';
 
 dotenv.config();
 
 const startServer = async () => {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      name: 'qid',
-      secret: 'SESSION_SECRET',
-      resave: true,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
-      },
-    }),
-  );
   await app.listen(process.env.PORT || 3001);
   console.log(`Server has started on the URL: ${await app.getUrl()}`);
 };
